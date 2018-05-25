44e7ab13582e9d9939d36057eb93e5360b95f56c
kiyon <kiyonlin@163.com>
2018-05-25 09:39:32 +0800

    d03 icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { MatIconRegistry } from '@angular/material';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor(private matIconRegistry: MatIconRegistry) { }
+
+  ngOnInit() {
+    this.matIconRegistry.registerFontClassAlias('fontawesome', 'fa');
+  }
 }
--- src/app/shared-material/shared-material.module.ts
+++ src/app/shared-material/shared-material.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MatButtonModule } from '@angular/material';
+import { MatButtonModule, MatIconModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   exports: [
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ],
   declarations: []
 })
