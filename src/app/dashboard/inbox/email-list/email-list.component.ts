f3fbd858589a2a4f61820dc9ffb2262cc5f6a0c1
kiyon <kiyonlin@163.com>
2018-05-29 14:10:24 +0800

    d24 table advanced

--- src/app/dashboard/inbox/email-list/email-list.component.ts
+++ src/app/dashboard/inbox/email-list/email-list.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { MatTableDataSource, MatPaginator, PageEvent, MatSort, Sort } from '@angular/material';
+import { MatTableDataSource, MatPaginator, PageEvent, MatSort, Sort, MatPaginatorIntl } from '@angular/material';
+import { fromEvent } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-email-list',
@@ -10,12 +12,17 @@ import { MatTableDataSource, MatPaginator, PageEvent, MatSort, Sort } from '@ang
 export class EmailListComponent implements OnInit {
   @ViewChild('paginator') paginator: MatPaginator;
   @ViewChild('sortTable') sortTable: MatSort;
+  @ViewChild('filter') filter: ElementRef;
+  currentFilterData: string;
 
   currentPage: PageEvent;
   currentSort: Sort;
   emailsDataSource = new MatTableDataSource<any>();
   totalCount: number;
-  constructor(private httpClient: HttpClient) { }
+  constructor(
+    private httpClient: HttpClient,
+    private matPaginatorIntl: MatPaginatorIntl
+  ) { }
 
   ngOnInit() {
     this.currentPage = {
@@ -34,6 +41,36 @@ export class EmailListComponent implements OnInit {
       this.currentPage = page;
       this.getIssuees();
     });
+
+    // 設定顯示筆數資訊文字
+    this.matPaginatorIntl.getRangeLabel = (page: number, pageSize: number, length: number): string => {
+      if (length === 0 || pageSize === 0) {
+        return `第 0 筆、共 ${length} 筆`;
+      }
+
+      length = Math.max(length, 0);
+      const startIndex = page * pageSize;
+      const endIndex = startIndex < length ? Math.min(startIndex + pageSize, length) : startIndex + pageSize;
+
+      return `第 ${startIndex + 1} - ${endIndex} 筆、共 ${length} 筆`;
+    };
+
+    // 設定其他顯示資訊文字
+    this.matPaginatorIntl.itemsPerPageLabel = '每頁筆數：';
+    this.matPaginatorIntl.nextPageLabel = '下一頁';
+    this.matPaginatorIntl.previousPageLabel = '上一頁';
+
+    fromEvent(this.filter.nativeElement, 'keyup')
+      .pipe(
+        debounceTime(300),
+        distinctUntilChanged()
+      ).subscribe(() => {
+        // 準備要提供給API的filter資料
+        this.currentFilterData = (this.filter.nativeElement as HTMLInputElement).value;
+        this.getIssuees();
+        // 後端篩選就不需要指定filter了
+        // this.emailsDataSource.filter = (this.filter.nativeElement as HTMLInputElement).value;
+      });
   }
 
   changeSort(sortInfo: Sort) {
@@ -50,6 +87,11 @@ export class EmailListComponent implements OnInit {
     if (this.currentSort.direction) {
       targetUrl = `${targetUrl}&&sort=${this.currentSort.active}&order=${this.currentSort.direction}`;
     }
+    if (this.currentFilterData) {
+      targetUrl = `${targetUrl}&q=${this.currentFilterData}+in:title`;
+    }
+    // https://api.github.com/search/issues?q=repo:angular/material2&page=1&per_page=10&q=build+in:title
+    //  console.log(targetUrl);
     this.httpClient
       .get<any>(targetUrl)
       .subscribe(data => {
